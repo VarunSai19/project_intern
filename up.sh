00ebab8c9dfa3f3fe064eaa464622993a1b791a0
varunsai99 <varunmessi@gmail.com>
2021-06-28 15:21:04 +0530

    fc

---
+++ up.sh
@@ -0,0 +1,28 @@
+rm -rf project_intern
+nvm install v10.15.3 
+git clone https://github.com/VarunSai19/project_intern.git
+sleep 3
+docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
+sleep 3
+chmod a+w project_intern
+cd project_intern
+cd artifacts/channel/create-certificate-with-ca
+docker-compose up -d
+sleep 3
+./create-certificate-with-ca.sh 
+cd ..
+./create-artifacts.sh 
+cd ..
+docker-compose up -d
+sleep 3
+cd ..
+./createChannel.sh 
+sleep 3
+./deployChaincode.sh 
+sleep 3
+cd api-2.0/config/
+./generate-ccp.sh 
+sleep 3
+cd ..
+sleep 3
+npm install --unsafe-perm
